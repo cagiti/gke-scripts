0767320c6be5464f0cee8c8d59dd84d9fffb3dc6
Gareth Evans <gareth@bryncynfelin.co.uk>
2018-12-20 16:21:53 +0000

    Added scripts to debug/fix cluster access

---
+++ debug_user_accounts.groovy
@@ -0,0 +1,25 @@
+#!/usr/bin/env groovy
+
+@Grapes([
+	@Grab('org.yaml:snakeyaml:1.17')
+])
+
+import org.yaml.snakeyaml.Yaml
+
+String.metaClass.yaml = { new Yaml().load(delegate) }
+
+def list = []
+
+def users = "kubectl get users -oyaml".execute().text.yaml()
+users.items.each{ user -> 
+	def binding = "kubectl get clusterrolebinding ${user.spec.serviceAccount} -oyaml".execute().text.yaml()
+	list << [name: user.spec.name, email: user.spec.email, role: binding.roleRef.name, sa: user.spec.serviceAccount]
+}
+
+println "Viewers"
+println "----------------------------------------"
+list.findAll{ it.role == "view" }.each{ println it }
+
+println "\n\nAdmin"
+println "----------------------------------------"
+list.findAll{ it.role == "cluster-admin" }.each{ println it }
--- fix_user_account.groovy
+++ fix_user_account.groovy
@@ -6,46 +6,59 @@
 
 import org.yaml.snakeyaml.Yaml
 
+String.metaClass.yaml = { new Yaml().load(delegate) }
+
+def list = []
+
+def users = "kubectl get users -oyaml".execute().text.yaml()
+users.items.each{ user -> 
+	def binding = "kubectl get clusterrolebinding ${user.spec.serviceAccount} -oyaml".execute().text.yaml()
+	list << [name: user.spec.name, email: user.spec.email, role: binding.roleRef.name, sa: user.spec.serviceAccount]
+}
+
+println "Viewers"
+println "----------------------------------------"
+list.findAll{ it.role == "view" }.each{ println it }
+
+println "\n\nAdmin"
+println "----------------------------------------"
+list.findAll{ it.role == "cluster-admin" }.each{ println it }
+
 def reservedEmails = ["wrefvem@cloudbees.com", "rawlingsj80@gmail.com", "james.strachan@gmail.com", "cosmin.cojocar@gmx.ch", "pmuir@cloudbees.com", "gareth@bryncynfelin.co.uk"]
 
-def yamlRaw = "kubectl get users -oyaml".execute().text
-Yaml parser = new Yaml()
-
-def users = parser.load(yamlRaw).items.collect { [key: it.spec.serviceAccount, email: it.spec.email ] }
-
-def usersToUpdate = users.findAll{ it.key }.findAll{ !reservedEmails.contains(it.email) }
+def usersToUpdate = list.findAll{ it.role == 'cluster-admin' }.findAll{ !reservedEmails.contains(it.email) }
 
 usersToUpdate.each{ user ->
-	println "kubectl delete clusterrolebinding ${user.key}".execute().text
-	println "kubectl delete rolebinding ${user.key}".execute().text
+	println "kubectl delete clusterrolebinding ${user.sa}".execute().text
+	println "kubectl delete rolebinding ${user.sa}".execute().text
 	def filePath = ""
-	File.createTempFile("temp-${user.key}",".yaml").with {
+	File.createTempFile("temp-${user.sa}",".yaml").with {
     	deleteOnExit()
 	    write """---
 apiVersion: rbac.authorization.k8s.io/v1
 kind: ClusterRoleBinding
 metadata:
-  name: ${user.key}
+  name: ${user.sa}
 roleRef:
   apiGroup: rbac.authorization.k8s.io
   kind: ClusterRole
   name: view
 subjects:
 - kind: ServiceAccount
-  name: ${user.key}
+  name: ${user.sa}
   namespace: jx
 ---
 apiVersion: rbac.authorization.k8s.io/v1
 kind: RoleBinding
 metadata:
-  name: ${user.key}
+  name: ${user.sa}
 roleRef:
   apiGroup: rbac.authorization.k8s.io
   kind: Role
   name: jx-view
 subjects:
 - kind: ServiceAccount
-  name: ${user.key}
+  name: ${user.sa}
   namespace: jx
 """
     	filePath = absolutePath
