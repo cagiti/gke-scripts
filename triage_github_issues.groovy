9d738eaf3ea6b7eb0167762c366820ec1afb7643
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-01-16 23:17:15 +0000

    Switched to using curl rather than toURL for requests

--- triage_github_issues.groovy
+++ triage_github_issues.groovy
@@ -4,6 +4,7 @@
 	@Grab('org.codehaus.groovy.modules.http-builder:http-builder:0.7.1')
 ])
 
+import groovy.time.*
 import groovy.json.*
 import org.yaml.snakeyaml.Yaml
 import groovyx.net.http.HTTPBuilder
@@ -20,6 +21,10 @@ String.metaClass.encode = {
 	java.net.URLEncoder.encode(delegate, "UTF-8")
 }
 
+String.metaClass.toDate {
+	Date.parse("yyyy-MM-dd'T'HH:mm", delegate)
+}
+
 Yaml parser = new Yaml()
 
 def home = new File('/Users/garethjevans')
@@ -29,27 +34,47 @@ def gitAuth = git.'github.com'.get(0)
 def issues = []
 def moreIssues = true
 def page = 1
+def per_page = 100
+
 while (moreIssues) {
-	def ghIssues = "https://${gitAuth.user}:${gitAuth.oauth_token}@api.github.com/repos/jenkins-x/jx/issues?per_page=100&page=${page}".toURL().text.json()
+	println "Getting page ${page}"
+	def ghIssues = "curl https://${gitAuth.user}:${gitAuth.oauth_token}@api.github.com/repos/jenkins-x/jx/issues?state=all&per_page=${per_page}&page=${page}".execute().text.json()
 	ghIssues.findAll{ !it.pull_request }.each{ 
 		def issueKind = it.labels.find{ label -> label.name.startsWith('kind')}?.name
 		def issuePriority = it.labels.find{ label -> label.name.startsWith('priority')}?.name
 		def issueArea = it.labels.find{ label -> label.name.startsWith('area')}?.name
-		issues << [id:it.html_url, title: it.title, state:it.state, kind:issueKind, priority:issuePriority, area:issueArea]
+		issues << [id:it.html_url, title: it.title, state:it.state, kind:issueKind, priority:issuePriority, area:issueArea, created_at: it.created_at?.toDate(), closed_at: it.closed_at?.toDate()]
 	}
 
 	page++
 
-	if (ghIssues.size() < 100) {
+	if (ghIssues.size() < per_page) {
 		moreIssues = false
 	}
 }
 
-issues.findAll{ !it.priority }.each{ println it }
+issues.findAll{ it.state == "open" }.findAll{ !it.priority }.each{ println it }
 
 def numberOfIssues = issues.size()
-def toTriage = issues.findAll{ !it.priority }.size()
+def numberOfOpenIssues = issues.findAll{ it.state == "open" }.size()
+def numberOfOpenIssuesExcludingEnhancements = issues.findAll{ it.state == "open" }.findAll{ it.kind != "kind/enhancement" }.size()
+def toTriage = issues.findAll{ it.state == "open" }.findAll{ !it.priority }.size()
 
+def timeToResolve = issues.findAll{ it.state == 'closed' }.findAll{ it.kind != "kind/enhancement" }.collect{ 
+	TimeCategory.minus(it.closed_at, it.created_at).days
+}
+println ""
+println "Average time to close ${timeToResolve.sum()/timeToResolve.size()} day(s)"
+
+println ""
 println "Total issue(s) ${numberOfIssues}"
 println "To triage ${toTriage}"
-println "Triage ${((numberOfIssues-toTriage)/numberOfIssues)*100}%"
+println "Triage ${((numberOfOpenIssues-toTriage)/numberOfOpenIssues)*100}%"
+println "% of open issues ${((numberOfOpenIssuesExcludingEnhancements)/numberOfIssues)*100}%"
+println ""
+
+issues.findAll{ it.priority }.groupBy{ it.priority }.each{ k,v -> println "${k} - ${v.size()}" }
+println ""
+issues.findAll{ it.kind }.groupBy{ it.kind }.each{ k,v -> println "${k} - ${v.size()}" }
+//println ""
+//issues.findAll{ it.area }.groupBy{ it.area }.each{ k,v -> println "${k} - ${v.size()}" }
