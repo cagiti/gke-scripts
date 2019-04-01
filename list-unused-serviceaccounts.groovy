4bed13cc1579ec4d0e77625903bf942120e0b680
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-04-01 11:57:08 +0100

    updated script to include vault accounts

--- list-unused-serviceaccounts.groovy
+++ list-unused-serviceaccounts.groovy
@@ -5,18 +5,23 @@ String.metaClass.json = { new groovy.json.JsonSlurper().parseText(delegate) }
 def serviceAccounts = "gcloud iam service-accounts list --format=json".execute().text.json().
 	collect{ it.email }.
 	findAll{ it.startsWith("jx") }.
-	findAll{ !it.contains('vault') }.
-	findAll{ !it.contains('jxce') }.
+	findAll{ !it.contains('bdd') }.
 	findAll{ !it.contains('212') }
 
 println serviceAccounts.size()
 
 serviceAccounts.each { sa ->
-	def clusterName = sa.replaceAll('@jenkinsx-dev.iam.gserviceaccount.com','').replaceAll('^jx-','')
-	//println clusterName
+	def clusterName = sa.replaceAll('@jenkinsx-dev.iam.gserviceaccount.com','').
+		replaceAll('^jx-','').
+		replaceAll('-kaniko$','').
+		replaceAll('^vault-','').replaceAll('-sa$','')
+
+	println clusterName
 	def json = "gcloud container clusters list --filter=${clusterName} --format=json".execute().text.trim()
 	if (json == "[]") {
 		println sa
 		println "gcloud iam service-accounts delete ${sa} --quiet".execute().text
-    }
+    } else {
+		println "Cluster ${clusterName} exists"
+	}
 }
