ab0f71423f5ea4377386d6a735d34e33f3102989
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-06-19 13:54:38 +0100

    updated script for prod

--- create_node_pool.sh
+++ create_node_pool.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 CLUSTER=jenkins-x-infra-production
-NAME=pool-6
+NAME=pool-10
 DISK_SIZE=200
+DISK_TYPE=pd-ssd
 ZONE=europe-west1-b
 MIN_NODES=3
 MAX_NODES=10
-NUM_NODES=5
-MACHINE_TYPE=n1-highmem-8
+NUM_NODES=4
+MACHINE_TYPE=n1-highmem-4
 
 gcloud container node-pools create $NAME \
         --cluster=$CLUSTER \
         --disk-size=$DISK_SIZE \
+        --disk-type=$DISK_TYPE \
         --enable-autorepair \
         --enable-autoupgrade \
         --enable-autoscaling \
@@ -20,7 +22,6 @@ gcloud container node-pools create $NAME \
         --machine-type=$MACHINE_TYPE \
         --num-nodes=$NUM_NODES \
         --zone=$ZONE \
-        --preemptible \
         --scopes=https://www.googleapis.com/auth/compute,https://www.googleapis.com/auth/devstorage.full_control,https://www.googleapis.com/auth/service.management,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring,https://www.googleapis.com/auth/cloud-platform
 
 
