394e8a46ab0af93b739b5d139346ec1bae969b2b
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-08-20 12:26:59 +0100

    displaying all images in use

--- debug_images_in_use.groovy
+++ debug_images_in_use.groovy
@@ -10,23 +10,26 @@ String.metaClass.yaml = { new Yaml().load(delegate) }
 
 def pods = "kubectl get pods -oyaml".execute().text.yaml()
 images = pods.items.findAll{ pod -> 
-    !pod.metadata.labels.'created-by-prow'
+    //!pod.metadata.labels.'created-by-prow'
+    true
 }.collect{ pod ->
     pod.spec.containers.collect{ container -> 
 	[ app: pod.metadata.labels.app, image: container.image ]}
 }.flatten().unique()
 
-jxImages = images.
-	findAll{ image -> image.image.contains('builder') || image.image.contains('jx') }.
-	findAll{ image -> !image.image.contains('abayer') }.
-	findAll{ image -> !image.image.contains('jxui') }
+//jxImages = images.
+//	findAll{ image -> image.image.contains('builder') || image.image.contains('jx') }.
+//	findAll{ image -> !image.image.contains('abayer') }.
+//	findAll{ image -> !image.image.contains('jxui') }
 
-jxImages.each{ 
-	println "${it.app}"
-	if (it.image.contains('builder')) {
-		def version = "docker run ${it.image} jx --version".execute().text.trim()
-		println "\t${it.image} -> ${version}"
-	} else {
+images.groupBy{ it.app }.each{ k, v -> 
+	println "${k}"
+	//if (it.image.contains('builder')) {
+	//	def version = "docker run ${it.image} jx --version".execute().text.trim()
+	//	println "\t${it.image} -> ${version}"
+	//} else {
+	v.each {
 		println "\t${it.image}"
-	} 
+	}
+	//} 
 }
